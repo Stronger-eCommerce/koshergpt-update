bf71e523f413f8c23e104a0ed39a12332f62a7ce
Jay <86730095+StrongereCommerce@users.noreply.github.com>
2025-01-07 15:01:14 -0500

    Create message.js

---
+++ message.js
@@ -0,0 +1,8 @@
+"Subject": "Exciting News from KosherGPT!"
+"Body": """Dear Valued User,
+Get ready for some thrilling updates! :rocket: We're enhancing KosherGPT to bring you an even better experience. To make this happen, we’ll need to take a short break.
+:calendar: Mark your calendars! KosherGPT will be offline on January 7, from 9:00 PM to 11:00 PM. But don't worry, it's all part of our big leap forward!
+:key: Important: We're resetting your password to keep things secure. You'll find a temporary password in your inbox soon, and you can change it back whenever you like.
+:no_entry_sign: Note: We've temporarily removed Google Sign-In for a smoother transition.
+Thanks for being a part of our KosherGPT family! We're excited to unveil what we have in store for you!
+The KosherGPT Team"""
